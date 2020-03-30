671e6ce7b3937059f4d63a4439cd7ba94fbcdabf
Simone <simonelega.sl@gmail.com>
2020-03-30 17:55:13 +0200

    creazione controller admin e rotte

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 use App\Post;
 use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 class PostsTableSeeder extends Seeder
 {
     /**
@@ -11,8 +12,16 @@ class PostsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 10 ; $i++) { 
+            $newpost = new Post();
+            $newpost->user_id = 1;
+            $newpost->title=$faker->sentence(3);
+            $newpost->body=$faker->text(255);
+            $newpost->slug= Str::finish(Str::slug($newpost->title), rand(1, 1000000));
+            $newpost->save();
+        }
+
     }
 }
